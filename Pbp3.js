f6d68d5f4dc04916ee551c7ef50d77f233218690
RAYHANSYAH <114573108+RYHANSYAH@users.noreply.github.com>
2023-10-19 01:25:19 +0700

    Add files via upload

---
+++ Pbp3.js
@@ -0,0 +1,39 @@
+/*RAYHANSYAH
+20220040283
+TI22A*/
+
+const resto = {
+    NamaResto: "Nasi Padang Payakumbuah",
+    Lokasi: "BSD",
+    MinumanFavorit: "Es Teh Manis",
+    MakananFavorit1: "Rendang",
+    isVegan1: true
+  }
+console.log(resto)
+
+  console.log("=================================")
+
+  const restaurants = [
+    {
+        NamaResto: "Kang Yana",
+        Lokasi: "Parungkuda",
+        MinumanFavorit: "Es Jeruk",
+        MakananFavorit: "Gurame Bakar",
+        isVegan: false
+    },
+    {
+        NamaResto: "Empal gepuk Si Babah",
+        Lokasi: "Cbadak",
+        MinumanFavorit: "Lemon tea",
+        MakananFavorit: "Empal gepuk",
+        isVegan: true
+    },
+    {
+        NamaResto: "Restoran Alam Sunda",
+        Lokasi: "Sukabumi",
+        MinumanFavorit: "Es Doger",
+        MakananFavorit: "Sayur Asem",
+        isVegan: false
+    }
+  ]
+console.log(restaurants);
